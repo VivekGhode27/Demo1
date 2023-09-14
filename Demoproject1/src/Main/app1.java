c6bfec2410ce8debdb8638ba7f7a90e4924fc196
workv <workv@192.168.0.103>
2023-09-14 10:53:34 +0530

    my first commit

---
+++ Demoproject1/src/Main/app1.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class app1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ Demoproject1/src/Main/app2.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class app2 {
+	public static void main(String[] args)
+	{
+		int n=100;
+		
+		int n1=7;
+		
+		double a=n/(n1*10);
+		
+		System.out.println(a);
+	}
+}
